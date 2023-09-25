ab240ada256172fd25d040af61afb3004cd050ab
Aabill <domingoaaronbill@gmail.com>
2023-09-25 09:31:32 +0800

    update login form label transition

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,17 +6,23 @@
         @csrf
 
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+        <div x-data class="relative group" x-ref="loginEmail">
+            <x-input-label class="absolute top-2 left-2 cursor-text group-[.is-focused]:-top-2 group-[.is-focused]:left-4  group-[.is-focused]:rounded-md group-[.is-focused]:px-1 group-[.is-focused]:pb-[0.5px] group-[.is-focused]:text-[11px] group-[.is-focused]:bg-gray-800  transition-all ease-linear duration-100" for="email" :value="__('Email')" />
+            <x-text-input id="email" 
+							x-on:focus="$refs.loginEmail.classList.add('is-focused');" 
+							x-on:blur="$refs.loginEmail.classList.remove('is-focused')"
+							class="block mt-1 w-full " type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div x-data class="mt-4 relative group" x-ref="loginPassword">
+            <x-input-label for="password" :value="__('Password')" 
+						class="absolute top-2 left-2 cursor-text group-[.is-focused]:-top-2 group-[.is-focused]:left-4  group-[.is-focused]:rounded-md group-[.is-focused]:px-1 group-[.is-focused]:pb-[0.5px] group-[.is-focused]:text-[11px] group-[.is-focused]:bg-gray-900  transition-all ease-linear duration-100"/>
 
             <x-text-input id="password" class="block mt-1 w-full"
+						x-on:focus="$refs.loginPassword.classList.add('is-focused');" 
+						x-on:blur="$refs.loginPassword.classList.remove('is-focused');"
                             type="password"
                             name="password"
                             required autocomplete="current-password" />
